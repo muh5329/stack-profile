c3a76d3ddb76c8a88a4803e020a56cab3a14891d
Muneeb Haq <muh5329@gmail.com>
2025-03-17 23:45:12 -0400

    c More header work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import Image from 'next/image'
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -15,13 +16,21 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col justify-top bg-gradient-to-b from-[#e1e1e1] to-[#c9c9cf] text-black">
-        <div className="grid row-5 grid-flow-col gap-x-10 pt-5 ">
-            <div className="flex flex-row"> About me </div>
-            <div className="flex flex-row"> Skills </div>
-            <div className="flex flex-row"> Projects</div>
-            <div className="flex flex-row"> Socials</div>
+        <div className="grid row-5 grid-flow-col  pt-5 ">
+            <div className=" flex  pl-5">
+                <Image
+                  src="/header/computer.jpg"
+                  width={50}
+                  height={50}
+                  alt="Picture of computer"
+                />
+            </div>
+            <div className="flex flex-row justify-center"> About me </div>
+            <div className="flex flex-row justify-center"> Skills </div>
+            <div className="flex flex-row justify-center"> Projects</div>
+            <div className="flex flex-row justify-center"> Socials</div>
 
-            <div className="flex flex-row pl-40"> Resume </div>
+            <div className="flex flex-row justify-end pr-10"> Resume </div>
         </div>
 
         <div className="flex text-large">
